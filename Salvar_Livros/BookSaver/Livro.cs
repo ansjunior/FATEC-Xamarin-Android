d1644eed168dce07a9f78c0ec565e8947b3135fa
Anisio Junior <47663717+ansjunior@users.noreply.github.com>
2019-06-14 16:18:57 -0300

    Add files via upload

---
+++ Salvar_Livros/BookSaver/Livro.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookSaver
+{
+    class Livro
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Autor { get; set; }
+    }
+}
---
+++ Salvar_Livros/BookSaver/NovoLivro.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BookSaver
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NovoLivro : ContentPage
+	{
+		public NovoLivro ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Livro livro = new Livro()
+            {
+                Nome = livroEntry.Text,
+                Autor = autorEntry.Text
+
+            };
+
+            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.DB_PATH))
+            { 
+            conn.CreateTable<Livro>();
+            var numeroDeLinhas = conn.Insert(livro);
+
+                if(numeroDeLinhas > 0)
+                    DisplayAlert("FEITO", "Livro e autor salvo!", "Ok");
+                else
+                    DisplayAlert("Erro", "Livro não foi inserido!", "Ok");
+            }
+
+
+        }
+    }
+}
